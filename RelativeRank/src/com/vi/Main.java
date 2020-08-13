93372a1aed72af0c55f5109e5fe17966f49513e7
victor <touchwithvictor@gmail.com>
2020-08-13 23:24:59 +0530

    GFG

---
+++ RelativeRank/src/com/vi/Main.java
@@ -0,0 +1,11 @@
+package com.vi;
+
+import java.util.Arrays;
+
+//https://www.geeksforgeeks.org/find-relative-rank-of-each-element-in-array/
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println(Arrays.toString(Solution.rank(new int[]{8, 16, 5, 6, 9}, 5)));
+    }
+}
