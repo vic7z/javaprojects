9fb2c2e0648ad1bf44f99bc688164d229b908306
victor <touchwithvictor@gmail.com>
2020-09-07 20:58:40 +0530

    spring boot

---
+++ javaFx/untitled/src/com/vi/Main.java
@@ -0,0 +1,15 @@
+package com.vi;
+
+public class Main {
+
+    public static void main(String[] args) {
+        short s=3;
+        System.out.println(add(s,10));
+    }
+    static int add(int i,int j){
+        return i+j;
+    }
+
+	// write your code here
+
+}
